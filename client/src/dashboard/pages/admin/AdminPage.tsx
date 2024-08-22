49e743d18ce74f0838148a3bf6027c3be3fe5a92
Ahmed Hossam <128134339+Ahmed-Hossam-1@users.noreply.github.com>
2024-08-22 13:55:32 +0300

    rm client

--- client/src/dashboard/pages/admin/AdminPage.tsx
+++
@@ -1,5 +0,0 @@
-const AdminPage = () => {
-  return <div>dsvsfvmpom</div>;
-};
-
-export default AdminPage;
--- client/src/dashboard/pages/reports/Reports.page.tsx
+++
@@ -1,5 +0,0 @@
-const Reports_page = () => {
-  return <div>Reports_page</div>;
-};
-
-export default Reports_page;
